a10e85c88708256bf15ee12a15da6b35da49ff5a
Watsnaz <ryan@watsnaz.church>
2026-07-29 16:04:21 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.CALENDAR_WIDGET_CONFIG = {
   // Paste the Calendar ID from Google Calendar > Settings > Integrate calendar.
-  googleCalendarId: c_7e13b358b5556e04566b4552468a2a0e769a27fc9622b390c94bcaa2b248bccc@group.calendar.google.com,
+  googleCalendarId: "c_7e13b358b5556e04566b4552468a2a0e769a27fc9622b390c94bcaa2b248bccc@group.calendar.google.com",
 
   // Paste the restricted Google API key from Google Cloud Console.
-  googleApiKey: "AIzaSyBvzNlRGl7Sc0GZLsrcg5egQCmaBNXWcZw,
+  googleApiKey: "AIzaSyBvzNlRGl7Sc0GZLsrcg5egQCmaBNXWcZw",
 
   // Use an IANA time zone name. Watsonville is America/Los_Angeles.
   timeZone: "America/Los_Angeles",
